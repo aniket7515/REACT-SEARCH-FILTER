a269ec92f2fca366291f91c86718b8d445ca4c1c
Aniket <aniketdhokane2071@gmail.com>
2022-03-03 19:18:10 +0530

    React Basic Search Filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React,{useState}  from 'react';
 import './App.css';
+import {Users} from './users'
 
 function App() {
+  const [query,setQuery]= useState("");
+
+  console.log(Users.filter(user=> user.first_name.toLowerCase().includes("fe")));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+      type="text" 
+      placeholder="Search..." 
+      className="search" 
+      onChange={(e)=> setQuery(e.target.value)}
+      
+      />
+      <ul className="list">
+        {Users.filter((user)=> user.first_name.toLowerCase().includes(query)).map((user) =>(
+          <li key={user.id} className="listItem">{user.first_name}</li>
+
+        ))}
+
+        
+       
+      </ul>
+      
     </div>
   );
 }
